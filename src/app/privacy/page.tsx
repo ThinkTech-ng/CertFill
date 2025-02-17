9face0b9fa63b3079faec99cb9174d0d1927f157
Bwise1 <oguntoyebenjamin2@gmail.com>
2025-02-17 19:49:02 +0200

    terms and privacy

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,11 @@
+import PrivacyPolicy from "./privacy-policy";
+
+const PrivacyPage = () => {
+  return (
+    <div className="min-h-screen p-5">
+      <PrivacyPolicy />
+    </div>
+  );
+};
+
+export default PrivacyPage;
--- src/components/atom/terms.tsx
+++ src/components/atom/terms.tsx
@@ -5,12 +5,12 @@ export const TermAndCondition = ({ type }: { type: "register" | "login" }) => {
     <div className="text-sm text-center text-tin mt-6 py-4">
       By {type === "register" ? "creating an account," : "continuing,"} you
       agree with our{" "}
-      <Link href="#" className="underline hover:text-foreground text-onyx">
+      <Link href="/terms" className="underline hover:text-foreground text-onyx">
         Terms of Service
       </Link>{" "}
       and{" "}
       <Link
-        href="https://docs.google.com/document/d/18L1wYp5HiNtuV-binbYa-HKrA_6xPZJ70f8zQBh-9sI/edit"
+        href="/privacy"
         className="underline hover:text-foreground text-onyx"
       >
         Privacy Policy
